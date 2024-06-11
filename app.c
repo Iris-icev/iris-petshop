d9c213732ffca2a12fa8779f25933b436044c904
Marcos Valdecy Macedo Costa Leite <mvaldecy11@gmail.com>
2024-06-11 12:09:46 -0300

    fix: change menu logic and refactor code

--- app.c
+++ app.c
@@ -3,15 +3,17 @@
 - Na opção de cadastro o Algoritmo deverá permitir cadastrar no minimo 5 registros para todas as opções.
 - Também na opção de Cadastro a aplicação terá que gravar as informações num arquivo texto com o nome correspondente ao cadastro (Ex. se o cadastro for de produto, deverá conter um arquivo com o mesmo nome PRODUTO.TXT)
 - Aplicação deverá funcionar conforme os Menus que serão criados possibilitando o usuário executar as opçoes quantas vezes ele desejar.
-- CONTROLE DE PET SHOP,  
+- CONTROLE DE PET SHOP,
 */
 #include <stdio.h>
 
 float valor1, valor2, res;
-int operacao;
 int retorno;
+int operacaoMenu;
 
-void MenuPrincipal (){
+void MenuPrincipal()
+{
+    // 0
     printf("+===========================================+\n");
     printf("|                 Iris PetShop              |\n");
     printf("+===========================================+\n");
@@ -19,21 +21,27 @@ void MenuPrincipal (){
     printf("+===========================================+\n");
     printf("|    3-GERAR SERVIÇOS   |   4-FINANCEIRO    |\n");
     printf("+===========================================+\n");
-    printf("|    5-VENDA            |   5-SAIR          |\n");
+    printf("|    5-VENDA            |   6-SAIR          |\n");
     printf("+===========================================+\n");
-    scanf("%d", &operacao);
+    printf("|           INSIRA A OPÇÃO DESEJADA         |\n");
+    printf("+===========================================+\n");
+    scanf("%d", &operacaoMenu);
 }
 
-void Entrada(){
-     do {
-     printf("Digite o primeiro valor: \n");
-     retorno = scanf("%f", &valor1);
-     } while (retorno == 0);
-         
-     printf("Digite o segundo valor: \n");
-     scanf("%f", &valor2);
-} 
-void Cadastro(){
+void Entrada()
+{
+    do
+    {
+        printf("Digite o primeiro valor: \n");
+        retorno = scanf("%f", &valor1);
+    } while (retorno == 0);
+
+    printf("Digite o segundo valor: \n");
+    scanf("%f", &valor2);
+}
+void Cadastro()
+{
+    // 1
     printf("+===========================================+\n");
     printf("|                 CADASTRO                  |\n");
     printf("+===========================================+\n");
@@ -43,9 +51,10 @@ void Cadastro(){
     printf("+===========================================+\n");
     printf("|    5-ANIMAL          |   6-RETORNAR       |\n");
     printf("+===========================================+\n");
-    scanf("%d", &operacao);
 }
-void Impressão(){
+void Impressao()
+{
+    // 2
     printf("+===========================================+\n");
     printf("|                 IMPRESSÃO                 |\n");
     printf("+===========================================+\n");
@@ -55,19 +64,21 @@ void Impressão(){
     printf("+===========================================+\n");
     printf("|    5-ANIMAL          |   6-RETORNAR       |\n");
     printf("+===========================================+\n");
-    scanf("%d", &operacao);
 }
-void GerarServiços(){
+void GerarServicos()
+{
+    // 3
     printf("+==========================================================+\n");
     printf("|                     GERAR SERVIÇOS                       |\n");
     printf("+==========================================================+\n");
     printf("|       1-Criar OS         |     2-Imprimir Serviço        |\n");
     printf("+==========================================================+\n");
     printf("|                     5-RETORNAR                           |\n");
-    printf("+===========================================================+\n");
-    scanf("%d", &operacao);
+    printf("+==========================================================+\n");
 }
-void Financeiro(){
+void Financeiro()
+{
+    // 4
     printf("+===========================================+\n");
     printf("|                  FINANCEIRO               |\n");
     printf("+===========================================+\n");
@@ -75,57 +86,44 @@ void Financeiro(){
     printf("+===========================================+\n");
     printf("|       3-SALÁRIOS      |   4-ALUGUEL       |\n");
     printf("+===========================================+\n");
-    printf("|                    5-SAIR                 |\n");
+    printf("|                  5-RETORNAR               |\n");
     printf("+===========================================+\n");
-    scanf("%d", &operacao);
 }
-void Venda(){
+void Venda()
+{
+    // 5
     printf("+==============================================+\n");
     printf("|                     VENDA                    |\n");
     printf("+==============================================+\n");
     printf("|       1-GERAR VENDA   |   2-IMPRIMIR VENDAS  |\n");
     printf("+==============================================+\n");
-    printf("|                    5-SAIR                    |\n");
+    printf("|                  5-RETORNAR                  |\n");
     printf("+==============================================+\n");
-    scanf("%d", &operacao);
 }
-int main (){
-do{   
-    MenuCalculadora();
-    switch(operacao)
+int main()
+{
+    int menu = 0;
+    switch (menu)
     {
-        if(valor1 > 6 && valor1 < 0)
-        printf("insira uma tecla valida.");
+    case 0:
+        MenuPrincipal();
+        break;
     case 1:
-        system("cls");
-        Entrada();
         Cadastro();
         break;
     case 2:
-        system("cls");
-        Entrada();
-        Impressão();
+        Impressao();
         break;
     case 3:
-        system("cls");
-        Entrada();
-        GerarServiços();
+        GerarServicos();
         break;
     case 4:
-        system("cls");
-        Entrada();
-        Faturamento();
-        if(valor2 == 0){
-        printf("Divisor nulo. Insira numero valido.\n");
-        break;}
-        saida();
+        Financeiro();
         break;
     case 5:
+        Venda();
         break;
     default:
-        printf("Opcao invalida! \n");
         break;
     }
-}
-while(operacao != 6 );
 }
